bc98be5c4e1e469da8010aa9f6aa6bc3561fd287
Valentin <v@varb.me>
2022-04-22 15:08:09 +0300

    removed extra files

--- src/app/core/history.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserHistory, createMemoryHistory } from 'history';
-
-import { IS_BROWSER } from '../config';
-
-
-const history = IS_BROWSER ? createBrowserHistory() : createMemoryHistory();
-
-export default history;
