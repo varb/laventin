f5a6c3e7e95acd63e4c15ee0766d8213e125b393
Valentin <v@varb.me>
2022-04-29 22:08:41 +0300

    fixes

--- src/modules/HomeScreen/index.tsx
+++ src/modules/HomeScreen/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { trackList } from 'data/tracklist';
+import StreamingLinks from 'components/StreamingLinks';
+import SocialLinks from 'components/SocialLinks';
+import { PageWrap } from 'components/Layout/styles';
 import {
   Root,
   LinksList,
@@ -17,57 +20,50 @@ import {
   TrackArtwork,
   LastReleaseLabel,
 } from './styles';
-import { trackList } from '../../data/tracklist';
-import StreamingLinks from '../../components/StreamingLinks';
-import SocialLinks from '../../components/SocialLinks';
-import { PageWrap } from '../../components/Layout/styles';
 
 const filteredList = trackList.filter((item) => item.active);
 const lastRelease = filteredList[0];
 
 function HomeScreen() {
   return (
-    <Root>
-      <PageWrap>
-        <Logo />
+    <PageWrap>
 
-        <MainBgCover />
-        <LastReleaseInfo>
-          <LastReleaseLabel>Last release</LastReleaseLabel>
-          <LastReleaseTitle>{lastRelease.name}</LastReleaseTitle>
-          <LastReleaseAuthor>{lastRelease.artist}</LastReleaseAuthor>
-        </LastReleaseInfo>
+      <MainBgCover />
+      <LastReleaseInfo>
+        <LastReleaseLabel>Last release</LastReleaseLabel>
+        <LastReleaseTitle>{lastRelease.name}</LastReleaseTitle>
+        <LastReleaseAuthor>{lastRelease.artist}</LastReleaseAuthor>
+      </LastReleaseInfo>
 
-        <SectionWrapper>
-          <StreamingLinks trackInfo={lastRelease} />
-        </SectionWrapper>
+      <SectionWrapper>
+        <StreamingLinks trackInfo={lastRelease} />
+      </SectionWrapper>
 
-        <SubTitle>Discography</SubTitle>
-        <SectionWrapper>
-          <LinksList>
-            {filteredList.map((track, index) => (
-              <TrackLink
-                key={index}
-                to={`/t/${track.id}`}
-                as={Link}
-              >
-                <TrackArtwork src={`/art/${track.id}/artwork.jpg`} />
-                <div>
-                  <TrackTitle>{track.name}</TrackTitle>
-                  <TrackArtist>{track.artist}</TrackArtist>
-                </div>
-              </TrackLink>
-            ))}
-          </LinksList>
-        </SectionWrapper>
+      <SubTitle>Discography</SubTitle>
+      <SectionWrapper>
+        <LinksList>
+          {filteredList.map((track, index) => (
+            <TrackLink
+              key={index}
+              to={`/t/${track.id}`}
+              as={Link}
+            >
+              <TrackArtwork src={`/art/${track.id}/artwork.jpg`} />
+              <div>
+                <TrackTitle>{track.name}</TrackTitle>
+                <TrackArtist>{track.artist}</TrackArtist>
+              </div>
+            </TrackLink>
+          ))}
+        </LinksList>
+      </SectionWrapper>
 
-        <SubTitle>Socials</SubTitle>
-        <SectionWrapper>
-          <SocialLinks />
-        </SectionWrapper>
+      <SubTitle>Socials</SubTitle>
+      <SectionWrapper>
+        <SocialLinks />
+      </SectionWrapper>
 
-      </PageWrap>
-    </Root>
+    </PageWrap>
   )
 }
 
--- src/modules/HomeScreen/styles.ts
+++ src/modules/HomeScreen/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { H2 } from '../../components/Typography';
+import { H2 } from 'components/Typography';
 
 export const Root = styled.div`
   position: relative;
